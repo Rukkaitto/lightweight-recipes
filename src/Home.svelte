a393da348409fac1d8cffa284e5f81b93adc9bc2
Lucas Goudin <goudinlucas@gmail.com>
2021-03-07 14:15:06 +0100

    Added recipe searching

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,1 +1,28 @@
-<main>This is home.</main>
+<script>
+  import Recipe from "./components/Recipe.svelte";
+
+  const APP_ID = "f6a74723";
+  const API_URL = "https://api.edamam.com/search";
+  const API_KEY = "0bf969053b1ed8fa981b4e4fd8e85534";
+
+  let searchText = "";
+  let searchResults = [];
+
+  function search() {
+    fetch(`${API_URL}?app_id=${APP_ID}&app_key=${API_KEY}&q=${searchText}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        searchResults = response.hits;
+      });
+  }
+</script>
+
+<main>
+  <input bind:value={searchText} type="text" placeholder="Search recipes..." />
+  <button on:click={search}>Search</button>
+
+  {#each searchResults as searchResult}
+    <Recipe {...searchResult.recipe} />
+  {/each}
+</main>
---
+++ src/components/Recipe.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let image = "";
+  export let label = "";
+  export let totalTime = "";
+  export let ingredientLines = [];
+</script>
+
+<div id="recipe">
+  <img src={image} />
+  <div id="text">
+    <h2>
+      {label}
+    </h2>
+    {#if totalTime}
+      <h3>
+        {totalTime} minutes
+      </h3>
+    {/if}
+    <h4>Ingredients:</h4>
+    <ul>
+      {#each ingredientLines as ingredient}
+        <li>
+          {ingredient}
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
+
+<style>
+  #recipe {
+    display: flex;
+    flex-direction: row;
+    background-color: #d6daf0;
+    margin: 20px;
+  }
+  img {
+    margin: 20px;
+  }
+  #text {
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
+  }
+  ul {
+    padding-left: 20px;
+  }
+</style>
