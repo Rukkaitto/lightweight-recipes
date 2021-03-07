a064b531a88e733721787ee3a5d3bcc79d5fa1ed
Lucas Goudin <goudinlucas@gmail.com>
2021-03-07 12:07:18 +0100

    Added router

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,13 @@
 <script>
+  import Router from "svelte-spa-router";
+  import Home from "./Home.svelte";
+
+  const API_URL = "https://api.spoonacular.com/recipes/complexSearch";
+  const API_KEY = "6c144d0905ab4f17a33e32b01c86d9ea";
+
+  const routes = {
+    "/": Home,
+  };
 </script>
 
 <svelte:head>
@@ -6,7 +15,7 @@
 </svelte:head>
 
 <main>
-  <h1>Hello world!</h1>
+  <Router {routes} />
 </main>
 
 <style>
